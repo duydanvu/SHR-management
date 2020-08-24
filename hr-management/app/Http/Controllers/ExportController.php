84390ea25f520fa55dbab257805467600141896e
duydanvu <dangch15339@gmail.com>
2020-08-25 03:05:34 +0700

    25/08 sang

--- hr-management/app/Http/Controllers/ExportController.php
+++ hr-management/app/Http/Controllers/ExportController.php
@@ -19,34 +19,167 @@ class ExportController extends Controller implements FromCollection,WithHeadings
     //
     public function collection()
     {
-        if ($this->request->add_now_ex == null){
-            dd('dddd');
+        $users = User::join('stores','users.store_id','=','stores.store_id')
+            ->join('positions','users.position_id','=','positions.position_id')
+            ->join('contracts','users.contract_id','=','contracts.contract_id')
+            ->join('departments','users.department_id','=','departments.id')
+            ->join('services','users.service_id','=','services.id')
+            ->select('stores.store_address','positions.description as ps_description'
+                ,'contracts.description as ct_description','departments.description as dp_description',
+                'services.description as sv_description')
+            ->where('users.store_id','=',$this->request->store_export)
+            ->where('users.position_id','=',$this->request->position_export)
+            ->where('users.contract_id','=',$this->request->contract_export)
+            ->where('users.department_id','=',$this->request->department_export)
+            ->where('users.service_id','=',$this->request->service_export)
+            ;
+        $arr = [];
+        if ($this->request->name_ex != null){
+            $users = $users->addSelect('users.first_name','users.last_name');
+            array_push($arr,'first_name','last_name');
         }
-        dd($this->request);
-        $users = User::all();
-        foreach ($orders as $row) {
-            $order[] = array(
-                '0' => $row->id,
-                '1' => $row->name,
-                '2' => $row->address,
-                '3' => $row->email,
-                '4' => $row->order_date,
-                '5' => number_format($row->total),
-            );
+        if($this->request->email_ex != null){
+            $users = $users->addSelect('users.email');
+            array_push($arr,'email');
         }
-
+        if($this->request->phone_ex != null){
+            $users = $users->addSelect('users.phone');
+            array_push($arr,'phone');
+        };
+        if($this->request->dob_ex != null){
+            $users = $users->addSelect('users.dob');
+            array_push($arr,'dob');
+        };
+        if($this->request->gender_ex != null){
+            $users = $users->addSelect('users.gender');
+            array_push($arr,'gender');
+        };
+        if($this->request->line_ex != null){
+            $users = $users->addSelect('users.line');
+            array_push($arr,'line');
+        };
+        if($this->request->NContract_ex != null){
+            $users = $users->addSelect('users.contract_number');
+            array_push($arr,'contract_number');
+        };
+        if($this->request->start_time_ex != null){
+            $users = $users->addSelect('users.start_time');
+            array_push($arr,'start_time');
+        };
+        if($this->request->end_time_ex != null){
+            $users = $users->addSelect('users.end_time');
+            array_push($arr,'end_time');
+        };
+        if($this->request->idNumber_ex != null){
+            $users = $users->addSelect('users.identity_number');
+            array_push($arr,'identity_number');
+        };
+        if($this->request->tin_ex != null){
+            $users = $users->addSelect('users.tin');
+            array_push($arr,'tin');
+        };
+        if($this->request->idDate_ex != null){
+            $users = $users->addSelect('users.idn_date');
+            array_push($arr,'idn_date');
+        };
+        if($this->request->idAddress_ex != null){
+            $users = $users->addSelect('users.idn_address');
+            array_push($arr,'idn_address');
+        };
+        if($this->request->sscNumber_ex != null){
+            $users = $users->addSelect('users.ssc_number');
+            array_push($arr,'ssc_number');
+        };
+        if($this->request->hospital_ex != null){
+            $users = $users->addSelect('users.hospital');
+            array_push($arr,'hospital');
+        };
+        if($this->request->ban_ex != null){
+            $users = $users->addSelect('users.ban');
+            array_push($arr,'ban');
+        };
+        if($this->request->bank_ex != null){
+            $users = $users->addSelect('users.bank');
+            array_push($arr,'bank');
+        };
+        if($this->request->noi_add_ex != null){
+            $users = $users->addSelect('users.noi_address');
+            array_push($arr,'noi_address');
+        };
+        if($this->request->add_now_ex != null){
+            $users = $users->addSelect('users.address_now');
+            array_push($arr,'address_now');
+        };
+        foreach ($users->get() as $item => $row) {
+            foreach ($arr as $key => $value) {
+                $order[] = array(
+                     $key => $row->$value,
+                );
+            }
+        }
         return (collect($order));
     }
 
     public function headings(): array
     {
-        return [
-            'id',
-            'Tên',
-            'Địa chỉ',
-            'Email',
-            'Ngày đặt hàng',
-            'Tổng',
-        ];
+        $arr = [];
+        if ($this->request->name_ex != null){
+            array_push($arr,'Họ','Tên');
+        }
+        if($this->request->email_ex != null){
+            array_push($arr,'Email');
+        }
+        if($this->request->phone_ex != null){
+            array_push($arr,'Phone');
+        };
+        if($this->request->dob_ex != null){
+            array_push($arr,'Ngày sinh');
+        };
+        if($this->request->gender_ex != null){
+            array_push($arr,'Giới tính');
+        };
+        if($this->request->line_ex != null){
+            array_push($arr,'Trình độ chuyên mon');
+        };
+        if($this->request->NContract_ex != null){
+            array_push($arr,'Số Hợp Đồng');
+        };
+        if($this->request->start_time_ex != null){
+            array_push($arr,'NGày bắt đầu');
+        };
+        if($this->request->end_time_ex != null){
+            array_push($arr,'Ngày kết thúc');
+        };
+        if($this->request->idNumber_ex != null){
+            array_push($arr,'Số CNT');
+        };
+        if($this->request->tin_ex != null){
+            array_push($arr,'Mã Số thuế cá nhân');
+        };
+        if($this->request->idDate_ex != null){
+            array_push($arr,'Ngày cấp CMT');
+        };
+        if($this->request->idAddress_ex != null){
+            array_push($arr,'Nơi cấp CMT');
+        };
+        if($this->request->sscNumber_ex != null){
+            array_push($arr,'Số bảo hiểm xã hội');
+        };
+        if($this->request->hospital_ex != null){
+            array_push($arr,'Nơi đăng ký khám');
+        };
+        if($this->request->ban_ex != null){
+            array_push($arr,'Số tài khoản ngân hàng');
+        };
+        if($this->request->bank_ex != null){
+            array_push($arr,'Tên ngân hàng');
+        };
+        if($this->request->noi_add_ex != null){
+            array_push($arr,'Địa chỉ thường trú');
+        };
+        if($this->request->add_now_ex != null){
+            array_push($arr,'Nơi ở hiện tại');
+        };
+        return $arr;
     }
 }
